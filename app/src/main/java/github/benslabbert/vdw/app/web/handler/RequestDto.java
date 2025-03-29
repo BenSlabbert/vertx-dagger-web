ef563a60e8db779bdef9633f39d00d9fe3f1be58
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-03-29 10:12:22 +0100

    fix build

--- app/src/main/java/github/benslabbert/vdw/app/web/handler/RequestDto.java
+++ app/src/main/java/github/benslabbert/vdw/app/web/handler/RequestDto.java
@@ -1,13 +1,30 @@
 /* Licensed under Apache-2.0 2024. */
 package github.benslabbert.vdw.app.web.handler;
 
+import com.google.auto.value.AutoBuilder;
+import github.benslabbert.vertxjsonwriter.annotation.JsonWriter;
 import io.vertx.core.json.JsonObject;
 import jakarta.validation.constraints.NotBlank;
 
+@JsonWriter
 public record RequestDto(@NotBlank @CheckString(strict = true) String data) {
 
   public static RequestDto fromJson(JsonObject jsonObject) {
-    String data = jsonObject.getString("data");
-    return new RequestDto(data);
+    return RequestDto_JsonWriter.fromJson(jsonObject);
+  }
+
+  public JsonObject toJson() {
+    return RequestDto_JsonWriter.toJson(this);
+  }
+
+  public static Builder builder() {
+    return new AutoBuilder_RequestDto_Builder();
+  }
+
+  @AutoBuilder
+  public interface Builder {
+    Builder data(String data);
+
+    RequestDto build();
   }
 }
--- app/src/main/java/github/benslabbert/vdw/app/web/handler/ResponseDto.java
+++ app/src/main/java/github/benslabbert/vdw/app/web/handler/ResponseDto.java
@@ -1,13 +1,29 @@
 /* Licensed under Apache-2.0 2024. */
 package github.benslabbert.vdw.app.web.handler;
 
+import com.google.auto.value.AutoBuilder;
+import github.benslabbert.vertxjsonwriter.annotation.JsonWriter;
 import io.vertx.core.json.JsonObject;
 
+@JsonWriter
 public record ResponseDto(String data) {
 
+  public static ResponseDto fromJson(JsonObject jsonObject) {
+    return ResponseDto_JsonWriter.fromJson(jsonObject);
+  }
+
   public JsonObject toJson() {
-    JsonObject json = new JsonObject();
-    json.put("data", data);
-    return json;
+    return ResponseDto_JsonWriter.toJson(this);
+  }
+
+  public static ResponseDto.Builder builder() {
+    return new AutoBuilder_ResponseDto_Builder();
+  }
+
+  @AutoBuilder
+  public interface Builder {
+    Builder data(String data);
+
+    ResponseDto build();
   }
 }
