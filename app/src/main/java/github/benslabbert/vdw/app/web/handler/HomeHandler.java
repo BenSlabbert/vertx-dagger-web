08b494c0ff69cff459b8806e0db9463135f9eed4
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-04-26 11:45:37 +0200

    fix build

--- app/src/main/java/github/benslabbert/vdw/app/web/handler/HomeHandler.java
+++ app/src/main/java/github/benslabbert/vdw/app/web/handler/HomeHandler.java
@@ -1,6 +1,9 @@
 /* Licensed under Apache-2.0 2024. */
 package github.benslabbert.vdw.app.web.handler;
 
+import github.benslabbert.txmanager.annotation.AfterCommit;
+import github.benslabbert.txmanager.annotation.BeforeCommit;
+import github.benslabbert.txmanager.annotation.Transactional;
 import github.benslabbert.vdw.codegen.annotation.HasRole;
 import github.benslabbert.vdw.codegen.annotation.WebHandler;
 import github.benslabbert.vdw.codegen.annotation.WebRequest;
@@ -9,13 +12,18 @@ import github.benslabbert.vdw.codegen.annotation.WebRequest.Get;
 import io.vertx.ext.web.RoutingContext;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @WebHandler(path = "/handler")
 class HomeHandler {
 
+  private static final Logger log = LoggerFactory.getLogger(HomeHandler.class);
+
   @Inject
   HomeHandler() {}
 
+  @Transactional
   @HasRole("admin")
   @Get(path = "/raw")
   void ctx(@WebRequest.RoutingContext RoutingContext ctx) {
@@ -33,5 +41,15 @@ class HomeHandler {
   @Get(path = "/data")
   void voidReturn(@Valid @Body RequestDto dto) {
     // do nothing
+  }
+
+  @BeforeCommit
+  private void beforeCommit() {
+    log.info("beforeCommit");
+  }
+
+  @AfterCommit
+  private void afterCommit() {
+    log.info("afterCommit");
   }
 }
