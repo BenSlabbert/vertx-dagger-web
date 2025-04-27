48c56fd20aabc30e78156cc3159efb60631af4cb
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-04-27 11:56:06 +0200

    bump bom

--- app/src/main/java/github/benslabbert/vdw/app/Main.java
+++ app/src/main/java/github/benslabbert/vdw/app/Main.java
@@ -1,12 +1,10 @@
 /* Licensed under Apache-2.0 2024. */
 package github.benslabbert.vdw.app;
 
+import github.benslabbert.vertxdaggercommons.launcher.CustomApplicationHooks;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 import io.netty.util.internal.logging.Slf4JLoggerFactory;
-import io.vertx.core.ThreadingModel;
-import io.vertx.launcher.application.HookContext;
 import io.vertx.launcher.application.VertxApplication;
-import io.vertx.launcher.application.VertxApplicationHooks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,30 +21,6 @@ public class Main extends VertxApplication {
   }
 
   private Main(String[] args) {
-    super(
-        args,
-        new VertxApplicationHooks() {
-          @Override
-          public void beforeDeployingVerticle(HookContext context) {
-            log.info("beforeDeployingVerticle");
-            // update deploymentOptions
-            context
-                .deploymentOptions()
-                .setThreadingModel(ThreadingModel.VIRTUAL_THREAD)
-                .setWorkerPoolSize(1)
-                .setWorkerPoolName("worker-pool");
-          }
-
-          @Override
-          public void beforeStartingVertx(HookContext context) {
-            log.info("beforeStartingVertx");
-            // update vertxOptions
-            context
-                .vertxOptions()
-                .setWorkerPoolSize(1)
-                .setEventLoopPoolSize(1)
-                .setInternalBlockingPoolSize(1);
-          }
-        });
+    super(args, new CustomApplicationHooks());
   }
 }
