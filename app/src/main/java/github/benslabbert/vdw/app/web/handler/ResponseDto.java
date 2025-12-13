aae2e0cfd1ae646c608f48fde644de71e899c176
benjamin <benjamin.slabbert@gmail.com>
2025-12-13 19:43:03 +0100

    mvn 4

--- app/src/main/java/github/benslabbert/vdw/app/web/handler/ResponseDto.java
+++ app/src/main/java/github/benslabbert/vdw/app/web/handler/ResponseDto.java
@@ -1,29 +1,9 @@
 /* Licensed under Apache-2.0 2024. */
 package github.benslabbert.vdw.app.web.handler;
 
-import com.google.auto.value.AutoBuilder;
-import github.benslabbert.vertxjsonwriter.annotation.JsonWriter;
-import io.vertx.core.json.JsonObject;
+import github.benslabbert.vdw.codegen.annotation.builder.GenerateBuilder;
+import github.benslabbert.vdw.codegen.annotation.json.JsonWriter;
 
 @JsonWriter
-public record ResponseDto(String data) {
-
-  public static ResponseDto fromJson(JsonObject jsonObject) {
-    return ResponseDto_JsonWriter.fromJson(jsonObject);
-  }
-
-  public JsonObject toJson() {
-    return ResponseDto_JsonWriter.toJson(this);
-  }
-
-  public static ResponseDto.Builder builder() {
-    return new AutoBuilder_ResponseDto_Builder();
-  }
-
-  @AutoBuilder
-  public interface Builder {
-    Builder data(String data);
-
-    ResponseDto build();
-  }
-}
+@GenerateBuilder
+public record ResponseDto(String data) {}
